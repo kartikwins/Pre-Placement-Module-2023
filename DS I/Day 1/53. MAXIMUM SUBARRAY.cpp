12bcf6acbd38d0d835b9c1a2c388bbe1547faed7
kartikwins <75691580+kartikwins@users.noreply.github.com>
2022-07-11 20:41:37 +0530

    Create 53. MAXIMUM SUBARRAY.cpp

---
+++ DS I/Day 1/53. MAXIMUM SUBARRAY.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) { 
+        int maxsum = nums[0];
+        int currsum = nums[0];
+        for (int i=1;i<nums.size();i++) {
+            currsum = max(nums[i], currsum + nums[i]);
+            if ( currsum>maxsum){
+                maxsum = currsum;
+            }
+        }
+            return maxsum;
+        
+    }
+};
