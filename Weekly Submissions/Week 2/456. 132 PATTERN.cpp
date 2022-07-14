f593aac41ff69ee55e83a811c0ca4243eb67ee02
kartikwins <75691580+kartikwins@users.noreply.github.com>
2022-07-14 21:59:14 +0530

    Create 456. 132 PATTERN.cpp

---
+++ Weekly Submissions/Week 2/456. 132 PATTERN.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+         int n=nums.size();
+    vector<int> leftMin(n);
+    leftMin[0] = nums[0];
+    for(int i=1;i<n;i++){
+        leftMin[i] = min(leftMin[i-1],nums[i]);
+    }
+    
+   for(int i=0;i<n;i++){
+        cout <<leftMin[i]<<"  ";}
+    
+  
+    stack<int> s;
+    for(int i=n-1;i>=0;i--){
+        if(nums[i] > leftMin[i]){
+            while(!s.empty() && s.top() <= leftMin[i]) s.pop();
+            if(!s.empty() && s.top() < nums[i]){
+                return true;
+            }
+            s.push(nums[i]);
+        }
+        
+    }
+    return false;
+    }
+};
